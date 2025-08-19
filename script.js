0b0e04bf73fd17dca714d2b23335b6c0a8161307
Vlinho <testedoxbox99@gmail.com>
2025-08-19 10:18:46 -0300

    New Animals Addition

--- script.js
+++ script.js
@@ -19,10 +19,43 @@ const pets = [
     idade: "Desconhecido", 
     imagem: "capybara.png" 
     },
+    { id: 4,
+      nome: "Carlos",
+      tipo: "Calopsita",
+      raca: "Amarelo",
+      idade: "6 meses",
+      imagem: "cockatiel.png"
+    },
+    { id: 5,
+      nome: "Beto",
+      tipo: "Hamster",
+      raca: "Sírio",
+      idade: "1 ano",
+      imagem: "hamster.png"
+    }
 
 ];
+
+// Essa forma de exibir o animal selecionado no select é ruim, pois envolve modificar
+// os arquivos HTML e JavaScript toda vez que um novo animal é adicionado !!!
 let cards = ""
 for (let i = 0; i < pets.length; i++) {
+    j = i+1;
+    k = i+2;
+    l = i+3;
+    m = i+4;
+    if (j >= pets.length) {
+        j -= pets.length;
+    }
+    if (k >= pets.length) {
+        k -= pets.length;
+    }
+    if (l >= pets.length) {
+        l -= pets.length;
+    }
+    if (m >= pets.length) {
+        m -= pets.length;
+    }
     cards += `
     <div class="card">
         <img src="${pets[i].imagem}" alt="${pets[i].nome}">
@@ -30,13 +63,9 @@ for (let i = 0; i < pets.length; i++) {
         <p>Tipo: ${pets[i].tipo}</p>
         <p>Idade: ${pets[i].idade}</p>
         <p>Raça: ${pets[i].raca}</p>
-        <button onclick="adotar('${pets[i].nome}', '${pets[i + 1]?.nome}', '${pets[i + 2]?.nome}')">Quero adotar</button>
+        <button onclick="adotar('${pets[i].nome}', '${pets[j]?.nome}', '${pets[k]?.nome}', '${pets[l]?.nome}', '${pets[m]?.nome}')">Quero adotar</button>
     </div>
     `
 }
 
 document.getElementById("dados").innerHTML = cards;
-
-function exibirAlgo(idPet) {
-  document.getElementById(idPet).value = idPet;
-}
